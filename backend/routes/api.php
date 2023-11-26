ea261f5d4091a17cf0bf315ac596c32a58f1db83
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-11-26 21:15:28 +0300

    feat[backend]: add auth api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,27 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+
+// Guest Auth Routes
+Route::post('auth/register', [AuthenticationController::class, 'register'])->middleware('guest');
+Route::post('auth/login', [AuthenticationController::class, 'login'])->middleware('guest');
+
+
+// Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::post('auth/logout', [AuthenticationController::class, 'logout'])->name('logout');
+
+    Route::get('users/me', [UserController::class, 'me'])->name('current-user');
+    Route::get('users', [UserController::class, 'index'])->name('list-users');
 });
